0302849c147df6248c6252be7432a9881373b11c
Phuong <zinzin.vung@gmail.com>
2023-11-07 19:59:30 +0700

    update api url

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://web72.onrender.com",
 })
 
 axiosInstance.interceptors.request.use((config) => {
